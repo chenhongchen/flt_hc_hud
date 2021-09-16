579afd228316a83ddbce5cc8ddc02cb18eb80e07
chenhongchen <412130100@qq.com>
2021-09-16 11:35:16 +0800

    完善代码

--- lib/hud/hc_hud.dart
+++ lib/hud/hc_hud.dart
@@ -51,9 +51,9 @@ class HCHud extends StatefulWidget {
     }
   }
 
-  static _HCHudState of(BuildContext context) {
+  static _HCHudState of(BuildContext? context) {
     var hudState = _HCHudState();
-    if(context == null) return hudState;
+    if (context == null) return hudState;
     try {
       hudState =
           context.findAncestorStateOfType<_HCHudState>() ?? _HCHudState();
